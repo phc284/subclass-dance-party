758eb1b6e6db30ef848bf7dec8c6a5346eba1e0d
Hack Reactor Student <student@hackreactor.com>
2017-08-21 20:03:41 -0500

    fadein and slide images

--- src/fadeDancer.js
+++ src/fadeDancer.js
@@ -1,6 +1,8 @@
 var fadeDancer = function (top, left) {
   Dancer.call(this, top, left);
 
+  this.setColor();
+
 };
 
 fadeDancer.prototype = Object.create(Dancer.prototype);
@@ -8,5 +10,17 @@ fadeDancer.prototype = Object.create(Dancer.prototype);
 fadeDancer.prototype.fade = function() {
 
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  this.$node.fadeIn(3000);
+};
 
+Dancer.prototype.setColor = function () {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    border:10px solid green;
+    border-radius:10px;
+    position:absolute;
+  };
+  this.$node.css(styleSettings);
 };
